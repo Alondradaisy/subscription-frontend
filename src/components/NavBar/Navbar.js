4e9c2fa5bf73f2987d35aec63147e81a1b4fb3b5
Alondradaisy <alondra.warren96@gmail.com>
2021-08-17 10:02:24 -0700

    move context folder to src location, export checkAuthCookie, navbar styling

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { NavLink, Link } from "react-router-dom";
 import axios from "axios";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -32,7 +33,33 @@ async function Navbar() {
     }
   }
 
-  return <div></div>;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/">Subscription Signup</Link>
+          </Typography>
+
+          <NavLink activeStyle={{ color: "red" }} exact to={navLinkTitleTwo}>
+            <Button color="inherit" style={{ color: "white" }}>
+              {navLinkDisplayOne}
+            </Button>
+          </NavLink>
+
+          <NavLink activeStyle={{ color: "red" }} exact t0={navLinkTitleTwo}>
+            <Button
+              color="inherit"
+              style={{ color: white }}
+              onClick={() => logoutButton()}
+            >
+              {navLinkDisplayTwo}
+            </Button>
+          </NavLink>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/components/hooks/checkAuthCookie.js
+++ src/components/hooks/checkAuthCookie.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import Cookie from "js-cookie";
 import jwtDecode from "jwt-decode";
 
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../../context/AuthContext";
 function CheckAuthCookie() {
   const { dispatch } = useContext(AuthContext);
 
