db597df05b0a5a1c1154b6b8f7a287f9e2233598
Alondradaisy <alondra.warren96@gmail.com>
2021-08-19 08:10:51 -0700

    use context to set the state of the user through AuthContext

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import { NavLink, Link } from "react-router-dom";
+import Cookies from "js-cookie";
+// ask about this -> import { withRouter } from "react-router-dom";
 import axios from "axios";
-import { Button } from "@material-ui/core";
+import { AuthContext } from "../../context/AuthContext";
+import CheckAuthCookie from "../hooks/checkAuthCookie";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,12 +20,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-async function Navbar() {
+async function Navbar(props) {
   const classes = useStyles();
+  const { userLogin } = CheckAuthCookie();
+
+  useEffect(() => {
+    userLogin();
+  }, []);
+
+  const {
+    state: { user },
+    dispatch,
+  } = useContext(AuthContext);
 
   async function logout() {
     try {
-      await axios.get("http://localhost:3002/api/users/logout");
+      await axios.get("http://localhost:3000/api/users/logout");
 
       dispatch({
         type: "LOG_OUT",
